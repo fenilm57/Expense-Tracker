390adb9969c3936f7eae759d45aeacea8eeb8f61
Fenil <fenilm57@gmail.com>
2022-10-05 13:49:52 -0400

    Edit button bottom sheet created

--- expense_app/lib/widget/custom_gridview.dart
+++ expense_app/lib/widget/custom_gridview.dart
@@ -3,14 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/categories_list.dart';
+import 'CustomElevatedButton.dart';
+import 'CustomTextField.dart';
 
-class CustomGridView extends StatelessWidget {
+class CustomGridView extends StatefulWidget {
   final int index;
   const CustomGridView({
     super.key,
     required this.index,
   });
 
+  @override
+  State<CustomGridView> createState() => _CustomGridViewState();
+}
+
+class _CustomGridViewState extends State<CustomGridView> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController budgetcontroller = TextEditingController();
+  bool impNote = false;
   @override
   Widget build(BuildContext context) {
     final categories =
@@ -22,7 +32,7 @@ class CustomGridView extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => SingleExpenseScreen(index: index)),
+                builder: (context) => SingleExpenseScreen(index: widget.index)),
           );
         },
         child: ClipRRect(
@@ -31,7 +41,9 @@ class CustomGridView extends StatelessWidget {
             footer: GridTileBar(
               leading: GestureDetector(
                 child: const Icon(Icons.edit, size: 30),
-                onTap: () {},
+                onTap: () {
+                  bottomSheetCategories(context);
+                },
               ),
               title: const SizedBox(
                 width: 50,
@@ -53,7 +65,7 @@ class CustomGridView extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    categories[index].name.toUpperCase(),
+                    categories[widget.index].name.toUpperCase(),
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 28,
@@ -61,7 +73,7 @@ class CustomGridView extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    categories[index].budget.toString(),
+                    categories[widget.index].budget.toString(),
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 28,
@@ -74,6 +86,80 @@ class CustomGridView extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Future<dynamic> bottomSheetCategories(BuildContext context) {
+    final provider = Provider.of<CatagoriesList>(context, listen: false);
+    return showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      builder: (context) {
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Add Category",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CustomTextField(
+                controller: namecontroller,
+                hintText: 'Category Name',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CustomTextField(
+                controller: budgetcontroller,
+                hintText: 'Budget (Optional)',
+                textInputType: TextInputType.number,
+              ),
+            ),
+            StatefulBuilder(
+              builder: (BuildContext context, StateSetter stateSetter) {
+                return Switch(
+                  value: impNote,
+                  onChanged: (val) {
+                    stateSetter(() {
+                      impNote = val;
+                    });
+                  },
+                );
+              },
+            ),
+            CustomElevatedButton(
+              text: 'Add Category',
+              onPressed: namecontroller.value.text.isNotEmpty
+                  ? () {
+                      double budget;
+                      if (budgetcontroller.text == '') {
+                        budget = 0;
+                      } else {
+                        budget = double.parse(budgetcontroller.text);
+                      }
+                      setState(() {
+                        provider.addCategories(
+                          name: namecontroller.text,
+                          budget: budget,
+                          imp: impNote,
+                        );
+                      });
+
+                      namecontroller.clear();
+                      budgetcontroller.clear();
+                      Navigator.pop(context);
+                    }
+                  : null,
+            ),
+          ],
+        );
+      },
     );
   }
 }
